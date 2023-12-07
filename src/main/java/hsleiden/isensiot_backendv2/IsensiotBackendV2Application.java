c973d141f26901d21fb07f2bc169b2c01fb4cf7d
Loek Appel <loek@appel-snijder.nl>
2023-12-07 13:43:53 +0100

    start app

---
+++ src/main/java/hsleiden/isensiot_backendv2/IsensiotBackendV2Application.java
@@ -0,0 +1,13 @@
+package hsleiden.isensiot_backendv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsensiotBackendV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IsensiotBackendV2Application.class, args);
+    }
+
+}
---
+++ src/test/java/hsleiden/isensiot_backendv2/IsensiotBackendV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package hsleiden.isensiot_backendv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsensiotBackendV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
