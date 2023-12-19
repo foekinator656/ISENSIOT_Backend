7fb702ec1d42c699a20dabf8fe9ec2aa52313504
Loek Appel <loek@appel-snijder.nl>
2023-12-19 14:28:15 +0100

    added request mappings to the AddressController, BatchController, FrituurController

--- src/main/java/hsleiden/isensiot_backend/controller/FrituurController.java
+++ src/main/java/hsleiden/isensiot_backend/controller/FrituurController.java
@@ -1,4 +1,16 @@
 package hsleiden.isensiot_backend.controller;
 
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/frituur")
 public class FrituurController {
+
+
 }
